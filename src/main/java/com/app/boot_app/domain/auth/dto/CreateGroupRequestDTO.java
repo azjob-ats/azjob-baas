4cb9b3c5c339513b93ff41c48f16583e035d4df1
Saulo McChelsom <saulomcchelsom@gmail.com>
2025-07-24 11:44:55 -0300

    fix: up

--- src/main/java/com/app/boot_app/domain/auth/dto/CreateGroupRequestDTO.java
+++ src/main/java/com/app/boot_app/domain/auth/dto/CreateGroupRequestDTO.java
@@ -10,12 +10,16 @@ import java.util.UUID;
 @Data
 @Schema(description = "Request DTO for creating a new group")
 public class CreateGroupRequestDTO {
-    @NotBlank
+
+    @NotBlank(message = "O name é obrigatório")
     @Schema(description = "The name of the group.", example = "Admins")
     private String name;
+   
+    @NotBlank(message = "O description é obrigatório")
     @Schema(description = "A brief description of the group.", example = "Group for administrators with full access")
     private String description;
-    @NotBlank
+    
+    @NotBlank(message = "O enterpriseId é obrigatório")
     @Schema(description = "The unique identifier of the enterprise to which the group belongs.", example = "c1d2e3f4-g5h6-7890-1234-567890abcdef")
     private UUID enterpriseId;
 }
